9934aa251f240809cedc05fd7144104aeec2859d
Brad <30415619+itzbradmc@users.noreply.github.com>
2019-07-26 09:18:14 +0100

    Start of movement and basic enemy
    The player will move in the direction of the mouse when pressing the left click. The enemies currently only have a random size and no movement.

---
+++ ProjectSussexGamesDev/Assets/Brad/Scripts/Enemy_Basic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Basic : MonoBehaviour
+{
+
+    public float size = 0;
+
+    Vector2 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = Random.Range(1, 50);
+        transform.localScale = new Vector3((size / 50) * 2, (size / 50) * 2, (size / 50) * 2); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProjectSussexGamesDev/Assets/Brad/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float smoothRate = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            //Vector3 newPlayerPos = Vector3.Slerp(transform.position, mousePos, smoothRate);
+            //newPlayerPos.z = 0;
+            transform.position += -(transform.position - mousePos)/10;
+        }
+        
+    }
+}
