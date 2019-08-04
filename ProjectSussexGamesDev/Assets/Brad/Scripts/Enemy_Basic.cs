b2717b92c50b399f2299b22c119e44875d66cedc
Bradley WIlson <b.t.wilson@sky.com>
2019-08-04 17:48:09 +0200

    Basic enemy movement & Health loss & Growing
    Basic enemy movement:
    The enemy will move in a random direction for X amount of time then change.
    
    Health Loss:
    When the player touches an enemy their health goes down by 1 (starts a 3) and they cannot take damage for 2 seconds (shown by a blinking effec). The game restarts if their health is 0
    
    Growing:
    The players scale and the camers size change together, but the camera waits X amount of size increases to change

--- ProjectSussexGamesDev/Assets/Brad/Scripts/Enemy_Basic.cs
+++ ProjectSussexGamesDev/Assets/Brad/Scripts/Enemy_Basic.cs
@@ -6,19 +6,34 @@ public class Enemy_Basic : MonoBehaviour
 {
 
     public float size = 0;
+    [Range(0, 10)]
+    public float speed = 5;
 
-    Vector2 dir;
+    Vector3 dir;
+    float timer = 0;
+    float changeTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         size = Random.Range(1, 50);
-        transform.localScale = new Vector3((size / 50) * 2, (size / 50) * 2, (size / 50) * 2); 
+        transform.localScale = new Vector3((size / 50) * 2, (size / 50) * 2, (size / 50) * 2);
+        changeTime = Random.Range(0, 3);
+        speed = Random.Range(0, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Every X amount of time the direction is changed
+        if(timer >= changeTime){
+            dir = new Vector2(Random.Range(-100, 100), Random.Range(-100, 100)).normalized;
+            changeTime = Random.Range(0, 3);
+            timer = 0;
+        } else{
+            timer += Time.deltaTime;
+        }
+        //Moving the enemy
+        transform.position = Vector3.Slerp(transform.position, transform.position + dir, speed/100);
     }
 }
--- ProjectSussexGamesDev/Assets/Brad/Scripts/Player.cs
+++ ProjectSussexGamesDev/Assets/Brad/Scripts/Player.cs
@@ -4,27 +4,117 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float health = 3;
+    [Range(15,50)]
+    public float size = 0;
+    public int cameraIncreaseIncrement = 5; // how much the size needs to crease before the camera gets larger
+    public Camera camera;
 
-    public float smoothRate = 0.1f;
+    float invulnerabilityTime = 0;
+    float invulnerabilityTimer = 0;
+    bool canTakeDamage = true;
+    int previousSize = 0;
+    int sizeIncreaseDifference = 0;
+
+    SpriteRenderer sr;
+    private IEnumerator flashC;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        size = 15;
+        previousSize = (int) size;
+        camera.orthographicSize = 5 * (size / 25);
     }
 
     // Update is called once per frame
     void Update()
     {
+        sizeIncreaseDifference += Mathf.Abs((int)size - previousSize);
 
+        //When the player press left mouse they move towards the mouse position
         if (Input.GetMouseButton(0))
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
-            //Vector3 newPlayerPos = Vector3.Slerp(transform.position, mousePos, smoothRate);
-            //newPlayerPos.z = 0;
             transform.position += -(transform.position - mousePos)/10;
         }
-        
+
+        //If invulnerability is activated the player will not be able to take any damage
+        if(invulnerabilityTimer < invulnerabilityTime){
+            invulnerabilityTimer += Time.deltaTime;
+            canTakeDamage = false;
+        } else{
+            canTakeDamage = true;
+            sr.enabled = true;
+        }
+
+        //When the players health is 0 or less they will die
+        if(health <= 0){
+            Death();
+        }
+
+        //When the player has increase in size enough the camera will change size
+        if (sizeIncreaseDifference >= cameraIncreaseIncrement)
+        {
+            camera.orthographicSize = Vector3.Slerp(new Vector3(camera.orthographicSize, 0, 0), new Vector3(5 * (size / 25), 0, 0), 0.1f).x;
+            print(camera.orthographicSize + " " + 5 * (size / 25));
+            //Makes sure the camera is the correct size before resetting the counter
+            if(camera.orthographicSize >= (5 * (size / 25))-0.05f)
+            {
+                sizeIncreaseDifference = 0;
+            }
+
+        }
+        //The players size will chance immediatly
+        transform.localScale = new Vector3((size / 50) * 2, (size / 50) * 2, (size / 50) * 2);
+        previousSize = (int) size;
+    }
+
+    //Called when the player dies. It currently reloads the scene. This will need to be changed later
+    void Death(){
+        Application.LoadLevel(0);
+    }
+
+    //Removed X amount of health from the player
+    public void Damage(int amount){
+        health -= amount;
+    }
+
+    //Called when the player takes damage and activates the invulnerability
+    public void Invulnerability(float time){
+        invulnerabilityTime = time;
+        //Enables the blinking effect
+        flashC = Flash(time);
+        StartCoroutine(flashC);
+        Flash(time);
+    }
+
+    //When the player touches an enemy the Danger and Invulnerability functions are called
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Enemy_Basic>() != null)
+        {
+            Damage(1);
+            Invulnerability(2);
+        }
+    }
+
+    /*
+     * An IEnumerator is used so that other processs can run while the player is blinking
+     * instead of waiting until it has finished.
+     * The player swithced between 25% and 100% alpha every 0.1 seconds
+     */
+    IEnumerator Flash(float time)
+    {
+        for (int n = 0; n < time/0.2f; n++)
+        {
+            sr.color = new Color(255, 255, 255, 0.25f);
+            yield return new WaitForSeconds(0.1f);
+            sr.color = new Color(255, 255, 255, 1);
+            yield return new WaitForSeconds(0.1f);
+        }
+        StopCoroutine(flashC);
     }
 }
