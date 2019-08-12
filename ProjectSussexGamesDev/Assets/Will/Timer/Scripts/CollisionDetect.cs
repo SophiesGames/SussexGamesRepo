b710a0bf46dc9d01457f01a5e13c46666b315673
WILLSGAMINGPC\WillWatson <willwatson9999@gmail.com>
2019-08-12 13:52:42 +0100

    Added an endless timer. Currently, it disappears when you are killed, it does not reset when you respawn.

---
+++ ProjectSussexGamesDev/Assets/Will/Timer/Scripts/CollisionDetect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour
+{
+   
+    private void onTriggerEnter(Collider2D other)
+    {
+        GameObject.Find("player").SendMessage("Finish");
+    }
+}
