f878b667af35d74fc66d7840a20159e0c4b239b4
Sam Johnson <samwj02@gmail.com>
2019-08-30 08:50:41 +0100

    Menu scene is now functional apart from the options button, using placeholder buttons.

---
+++ ProjectSussexGamesDev/Assets/Sam/Scripts/PlayButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonHandler : MonoBehaviour
+{
+    public Button PlayButton;
+
+    void Start()
+    {
+        Button btn = PlayButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Game-Brad");
+        SceneManager.UnloadScene("Menu");
+    }
+}
---
+++ ProjectSussexGamesDev/Assets/Sam/Scripts/QuitButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuitButtonHandler : MonoBehaviour
+{
+    public Button QuitButton;
+
+    void Start()
+    {
+        Button btn = QuitButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("Works");
+        Application.Quit();
+    }
+}
