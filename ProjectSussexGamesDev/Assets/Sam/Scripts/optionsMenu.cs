ec9c28c89bf8d5050feb382e1eecd4f5978a4cde
Sam Johnson <samwj02@gmail.com>
2019-09-09 12:58:48 +0100

    Added (non-functional) volume and music sliders to options menu.

---
+++ ProjectSussexGamesDev/Assets/Sam/Scripts/optionsMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class optionsMenu : MonoBehaviour
+{
+    public AudioMixer musicMixer;
+    public AudioMixer soundEffectMixer;
+
+    public void SetMusicVolume (float volume)
+    {
+        musicMixer.SetFloat("musicVolume", volume);
+        Debug.Log(volume);
+    }
+
+    public void SetSoundEffectVolume (float volume)
+    {
+        soundEffectMixer.SetFloat("soundEffectVolume", volume);
+        Debug.Log(volume);
+    }
+}
